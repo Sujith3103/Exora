0959aa4c4a3160ed1d6f218dc6143380d60de5e5
Sujith MS <sujithsuji834@gmail.com>
2025-08-07 22:33:32 +0530

    feat(auth): setup login UI with styled input and labels

--- Client/src/components/layout/index.tsx
+++ Client/src/components/layout/index.tsx
@@ -3,10 +3,10 @@ import NavBar from "../navbar"
 
 const Layout = () => {
   return (
-    <div className="h-screen">
+    <div className="h-screen w-screen flex flex-col overflow-hidden">
       <NavBar/>
 
-      <div className="h-[92.2%] ">
+      <div className=" min-w-screen w-full flex-1">
         <Outlet />
       </div>
     </div>
--- Client/src/pages/student-view/auth/login.tsx
+++ Client/src/pages/student-view/auth/login.tsx
@@ -1,9 +1,39 @@
+import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Link } from "react-router-dom"
 
 const LoginComponent = () => {
   return (
-    <div className="bg-red-700 w-full h-full">
-      Login
-    </div>
+    <>
+      <form action="">
+        <div className="bg-white w-full h-full flex flex-col gap-7">
+          <div className="w-full shadow-[0_10px_25px_rgba(0,0,0,0.2)] rounded-xl bg-white flex p-2 gap-5  ">
+            <span className="w-1/15 bg-blue-200 rounded"></span>
+            <div className="flex-1">
+              <Label htmlFor="email" className="">Email</Label>
+              <Input id="email" type="email" placeholder="enter your email" className="border-none focus-visible:ring-0 focus-visible:ring-offset-0 p-0 h-7" />
+            </div>
+          </div>
+          <div className="w-full shadow-[0_10px_25px_rgba(0,0,0,0.2)] rounded-xl bg-white flex p-2 gap-5  ">
+            <span className="w-1/15 bg-blue-200 rounded"></span>
+            <div className="flex-1">
+              <Label htmlFor="password" className="font-">Password</Label>
+              <Input id="password" type="password" placeholder="password here" className="border-none focus-visible:ring-0 focus-visible:ring-offset-0 p-0 h-7" />
+            </div>
+          </div>
+          <div className="flex justify-around items-center">
+            <div className="flex items-center gap-2">
+              <Checkbox id="remember" className="border-blue-300 rounded-none border-2" />
+              <Label htmlFor="remember" className="text-blue-400 text-sm font-semibold">Remember Me</Label>
+            </div>
+            <Link to={'/auth/forgot-password'} className="text-blue-400 text-sm font-semibold">Forgot Password</Link>
+          </div>
+          <Button className="bg-blue-400 h-15 hover:bg-blue-300 text-md">Login</Button>
+        </div>
+      </form>
+    </>
   )
 }
 
