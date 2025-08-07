26183c867643ff1cd7c46dfc68d06c7e52876d83
Sujith MS <sujithsuji834@gmail.com>
2025-08-07 22:33:42 +0530

    feat(auth): setup signup UI with form fields and modern layout

--- Client/src/pages/student-view/auth/index.tsx
+++ Client/src/pages/student-view/auth/index.tsx
@@ -1,16 +1,18 @@
 import { Outlet } from "react-router-dom"
 
 const AuthPage = () => {
-  return (
-    <div className="w-full h-full p-20 flex bg-gradient-to-br from-indigo-900 via-purple-800 to-indigo-700 ">
-      <div className="bg-white w-1/2 h-full">
-        
-      </div>
-      <div className="h-full w-1/2">
-        <Outlet />  
-      </div>
-    </div>
-  )
+    return (
+        <div className="w-full h-full px-5 py-15  md:py-20 lg:px-50 flex bg-gradient-to-br from-indigo-900 via-purple-800 to-indigo-700 ">
+            <div className="lg:bg-white w-full h-full xl:p-10 flex justify-center xl:justify-end ">
+                
+                {/* image */}
+
+                <div className="h-full w-full bg-white p-8 rounded-xl shadow-2xl max-w-md xl:w-1/2">
+                    <Outlet />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default AuthPage
--- Client/src/pages/student-view/auth/signup.tsx
+++ Client/src/pages/student-view/auth/signup.tsx
@@ -1,8 +1,42 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 const SignUpComponent = () => {
+
+    const handleSubmit = async() => {
+        
+    }
+
     return (
-        <div className="bg-red-300 w-full h-full">
-            SignUpComponent
-        </div>
+        <>
+            <form>
+                <div className="bg-white w-full h-full flex flex-col gap-7">
+                    <div className="w-full shadow-[0_10px_25px_rgba(0,0,0,0.2)] rounded-xl bg-white flex p-2 gap-5  ">
+                        <span className="w-1/15 bg-blue-200 rounded"></span>
+                        <div className="flex-1">
+                            <Label htmlFor="name" className="">Username</Label>
+                            <Input id="name" type="text" placeholder="enter your Name" className="border-none focus-visible:ring-0 focus-visible:ring-offset-0 p-0 h-7" />
+                        </div>
+                    </div>
+                    <div className="w-full shadow-[0_10px_25px_rgba(0,0,0,0.2)] rounded-xl bg-white flex p-2 gap-5  ">
+                        <span className="w-1/15 bg-blue-200 rounded"></span>
+                        <div className="flex-1">
+                            <Label htmlFor="email" className="">Email</Label>
+                            <Input id="email" type="email" placeholder="enter your email" className="border-none focus-visible:ring-0 focus-visible:ring-offset-0 p-0 h-7" />
+                        </div>
+                    </div>
+                    <div className="w-full shadow-[0_10px_25px_rgba(0,0,0,0.2)] rounded-xl bg-white flex p-2 gap-5  ">
+                        <span className="w-1/15 bg-blue-200 rounded"></span>
+                        <div className="flex-1">
+                            <Label htmlFor="password" className="font-">Password</Label>
+                            <Input id="password" type="password" placeholder="password here" className="border-none focus-visible:ring-0 focus-visible:ring-offset-0 p-0 h-7" />
+                        </div>
+                    </div>
+                    <Button className="bg-blue-400 h-15 hover:bg-blue-300 mtext-d" onSubmit={handleSubmit}>Sign Up</Button>
+                </div>
+            </form> 
+        </>
     )
 }
 
